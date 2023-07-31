4941a1ccf29513f4b96ae355db3d0441730e637b
Dima311994 <vnuchok311994@hotmail.com>
2023-08-01 00:38:39 +0300

    feat: added reverse

--- src/index.js
+++ src/index.js
@@ -1,3 +1,16 @@
-module.exports = function reverse (n) {
-  
-}
+module.exports = function reverse(n) {
+    let str = n.toString();
+
+    if (str[0] === "-") {
+        str = str.slice(1);
+    }
+
+    const arr = str.split("");
+
+    let num = "";
+    for (let i = arr.length - 1; i >= 0; i--) {
+        num += arr[i];
+    }
+
+    return num;
+};
